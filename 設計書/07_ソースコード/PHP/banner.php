3c984a6c06171a067c125c1c5a3f23a20e3a80e0
Aso2001195 <83047112+Aso2001195@users.noreply.github.com>
2021-11-09 10:54:04 +0900

    Add files via upload

---
+++ 設計書/07_ソースコード/PHP/banner.php
@@ -0,0 +1,6 @@
+</head>
+<header class="header">
+    <p><h1 class="title">Aso Sports</h1></p>
+    <p><h6 class="sub_title">スポーツ用品:アソウスポーツ</h6></p>
+    <a href="TopPage.php"><button type="button" name="top_page">トップページ</button></a>
+</header>
