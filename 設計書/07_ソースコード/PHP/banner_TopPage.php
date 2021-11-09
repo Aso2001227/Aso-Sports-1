2cb429e02f72139fc9d2941fa4afabccfa209357
Aso2001195 <83047112+Aso2001195@users.noreply.github.com>
2021-11-09 11:02:45 +0900

    Add files via upload

---
+++ 設計書/07_ソースコード/PHP/banner_TopPage.php
@@ -0,0 +1,8 @@
+</head>
+<header class="header">
+    <p><h1 class="title">Aso Sports</h1></p>
+    <p><h6 class="sub_title">スポーツ用品:アソウスポーツ</h6></p>
+    <a href="TopPage.php"><button type="button" name="top_page">トップページ</button></a>
+    <a href="Login.php"><button type="button" name="login">ログイン</button></a>
+    <a href="Logout.php"><button type="button" name="login">ログアウト</button></a>
+</header>
