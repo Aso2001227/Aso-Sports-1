ef4f47d320b595bb2f4475e2c523e0e46efd13a4
Aso2001195 <83047112+Aso2001195@users.noreply.github.com>
2021-11-12 12:42:51 +0900

    Add files via upload

---
+++ 設計書/07_ソースコード/PHP/login.php
@@ -0,0 +1,16 @@
+<?php require 'header.php';?>
+<?php session_start() ?>
+    <title>ログイン</title>
+<?php require 'banner.php';?>
+        <p><h2>ログイン</h2></p>
+        <div class="log">
+            <form action="login-out.php" method="post">
+                <p>メールアドレス</p>
+                <input type="email" name="mail">
+                <p>パスワード</p>
+                <input type="password" name="pass">
+                <input type="submit" name="action_button" value="ログイン">
+                <a href="Member-Information-Registration.php">登録されてない方はこちら</a>
+            </form>
+        </div>
+<?php require 'footer.php';?>
